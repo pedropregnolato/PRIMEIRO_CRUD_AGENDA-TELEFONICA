b8f0b1dfb8a2602a1c2fe37672695f38ed8eb10a
Pedro Lucas Nunes Pregnolato <pregnolato.pedro@hcosta.com.br>
2021-12-22 10:54:42 -0300

    icones funcionando

--- primeiro_crud/index.php
+++ primeiro_crud/index.php
@@ -37,12 +37,12 @@
                 <div>
                     <h3>Buscar na agenda:</h3>
                     <input type="text" name="parametro" placeholder="Pesquisar"/>
-                    <button type="submit" ><img src="https://cdn-icons.flaticon.com/png/512/3031/premium/3031293.png?token=exp=1639668686~hmac=f52e00210c2d7b96d6efd18d5bcea332" alt="pesquisar" height="12px" border="0px"></button>
+                    <button type="submit" ><img src="imagens/pesquisar.png" alt="pesquisar" height="12px" border="0px"></button>
                 </div>
 
                 <br>
                 <!-- INSERT -->
-                <a href="paginanovocontato.html"><img src="https://cdn-icons.flaticon.com/png/512/3793/premium/3793621.png?token=exp=1639665684~hmac=768a718f399d1b1c937372e6f371e167" alt="adicionar_pessoa" height="21px" style="margin-top:10px; margin-left:6em"><br></a>
+                <a href="paginanovocontato.php"><img src="imagens/add.png" alt="adicionar_pessoa" height="21px" style="margin-top:10px; margin-left:6em"><br></a>
               
             </form>
         </p>
@@ -64,8 +64,8 @@
                             <td><?php echo $linha['id'] ?></td>
                             <td><?php echo $linha['nome'] ?></td>
                             <td><?php echo $linha['telefone'] ?></td>
-                            <td><a href="<?php echo "paginaalterar.php?id=" . $linha['id'] . "&nome=" . $linha['nome'] . "&telefone=" . $linha['telefone'] ?>"><img src="https://cdn-icons.flaticon.com/png/512/1633/premium/1633251.png?token=exp=1639663161~hmac=323b31e1b9270861f25b186a5cbd4e25" alt="editar" width="18px"></a></td>
-                            <td><a href="<?php echo "excluir.php?id=" . $linha['id'] ?>"><img src="https://cdn-icons.flaticon.com/png/512/1175/premium/1175343.png?token=exp=1639663106~hmac=7fb39639ae8c32b2986033aad0c35833" alt="lixeira" width="18px"></a></td>
+                            <td><a href="<?php echo "paginaalterar.php?id=" . $linha['id'] . "&nome=" . $linha['nome'] . "&telefone=" . $linha['telefone'] ?>"><img src="imagens/editar.png" alt="editar" width="18px"></a></td>
+                            <td><a href="<?php echo "excluir.php?id=" . $linha['id'] ?>"><img src="imagens/lixeira.png" alt="lixeira" width="18px"></a></td>
                         </tr>
 
                         <?php 
